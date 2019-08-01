acbfc46ff5663df9e7243bb37ccceeebbfe33452
Fred Clausen <fppc1@bitbucket.org>
2019-08-01 08:51:18 +0200

    John Cleese

--- prefix1.scala
+++ prefix1.scala
@@ -1,6 +1,6 @@
 
 object MyClass {
-// Fred Clausen
+// John Cleese
 /* The program ‘uniquePrefix’ examines a list of words and 
 returns the shortest unique prefix for each word. 
 It finds the shortest unique prefix by firstly building a list 
